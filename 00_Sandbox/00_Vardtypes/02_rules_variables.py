b086558d0cc98fcc9f9b94683a6c95026544fa03
Priyanshugitup <priyanshukk2025@gmail.com>
2026-03-12 16:23:57 +0530

    Variables Practice

---
+++ 00_Sandbox/00_Vardtypes/02_rules_variables.py
@@ -0,0 +1,6 @@
+a=23
+aaa=435
+sameer=45
+_sam=45
+# @sam = 56  # (it is invalid)
+# s@meer # invalid
---
+++ 00_Sandbox/00_Vardtypes/03_operators.py
@@ -0,0 +1,36 @@
+# Arithmetic Operators
+
+a=34
+b=4
+c=a+b
+print(c)
+
+# Assignment Operator
+a=4-2  # Assign 4-2 in a
+print(a)
+b=6
+b+=3 # Increament the value of b by 3 and then assign it  to b
+print(b)
+
+# Comparison Operators
+
+d = 5>=4
+print(d)  # will return boolean value i.e True or False
+
+# Logical Operators
+
+e = True or False
+# Truth table of 'or'
+print("True or False is ", True or False)
+print("False or True is ", False or True)
+print("True or True is ", True or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and'
+
+print("\nTrue and False is ", True and False)
+print("False and True is ", False and True)
+print("True and True is ", True and True)
+print("False and False is ", False and False)
+
+print(not(True))
