995a632c62acf04a6bf6d744a8b17c77a45d3bb6
Priyanshugitup <priyanshukk2025@gmail.com>
2026-03-13 19:39:47 +0530

    Variables Practice

---
+++ 00_Sandbox/00_Vardtypes/04_type.py
@@ -0,0 +1,14 @@
+a = 31
+t = type(a)
+print(t)
+
+b="Harry"
+s=type(b)
+print(s)
+
+c="31.2"
+u=type(c)
+print(u)
+d=float(c)
+v=type(d)
+print(v)
---
+++ 00_Sandbox/00_Vardtypes/05_inputs.py
@@ -0,0 +1,16 @@
+# a = input("Enter number 1: ") # taking input in form of string
+# b= input("Enter number 2: ") # taking input in form of string
+
+# print("Number a is: ",a)
+# print("Number a is: ",b)
+
+# print("Sum is: ",a+b) # concatenating the string
+
+
+a = int(input("Enter number 1: ")) # taking input in form of int
+b= int(input("Enter number 2: ")) # taking input in form of int
+
+print("Number a is: ",a)
+print("Number a is: ",b)
+
+print("Sum is: ",a+b)
