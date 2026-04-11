5a26c4a069f5e517d426de0f928fed8ec0e4ac24
Priyanshugitup <priyanshukk2025@gmail.com>
2026-04-11 20:06:03 +0530

    Exp - 9

---
+++ 03_Assignments/Experiment_9/q04.py
@@ -0,0 +1,23 @@
+class Shape:
+    def area(self):
+        return 0
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+circle = Circle(5)
+square = Square(4)
+
+print(f"Circle Area: {circle.area()}")
+print(f"Square Area: {square.area()}")
